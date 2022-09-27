82e924e53ffc6d25aed6c4028f80a1a19e8c2529
davidmgudeman <davidmgudeman@gmail.com>
2022-09-27 11:21:58 -0700

    remove item from cart implemented

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import {useSelector} from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-
+import { useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
-    const showCart = useSelector(state => state.ui.cartIsVisible);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
 
-
-   
-   
   return (
     <Layout>
-     {showCart && <Cart />}
+      {showCart && <Cart />}
       <Products />
-      
     </Layout>
   );
 }
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,13 @@
+import { useDispatch } from 'react-redux';
 import classes from './CartItem.module.css';
+import { cartActions } from '../../store/cart-slice';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { title, quantity, total, price, id } = props.item;
+  const dispatch = useDispatch();
+  const removeItemHandler = () => {
+      dispatch(cartActions.removeItemFromCart(id))
+  }
 
   return (
     <li className={classes.item}>
@@ -17,7 +23,7 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
+          <button onClick={removeItemHandler}>-</button>
           <button>+</button>
         </div>
       </div>
